60bbb1961df723317302a606ca5b853e0d664677
kisolele <kisolele@gmail.com>
2020-10-30 10:27:56 +0100

    sfsdf

--- mysite/COVID_contacts/views.py
+++ mysite/COVID_contacts/views.py
@@ -3,8 +3,32 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("testujemy gita ajeee 333 <p>ssss</p>")
-    test 777
+    
 
-t1
-t2
-t3
+    kod do ldapa
+    fsd
+    fsd
+    fsdf
+    sdf
+    sdf
+    sdf
+    sdf
+    sd
+    fsd
+    fsd
+    f
+    sdf
+    sd
+    filtersdf
+    sd
+    f
+    sd
+    filtersdfsd
+    fsd
+    filtersdfsd
+    fsd
+    f
+    sdf
+    setfsd
+    sd
+    
