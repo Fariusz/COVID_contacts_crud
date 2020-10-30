d1a743209de985efce8387019eee5cb35bacd15b
kisolele <kisolele@gmail.com>
2020-10-30 09:37:54 +0100

    linia2ga

--- mysite/COVID_contacts/views.py
+++ mysite/COVID_contacts/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("testujemy gita ajeee 222")
+    return HttpResponse("testujemy gita ajeee 222 <p>ssss</p>")
+    test jest linia 2
